fa9421f27401bf5a59dd9980bc035a9f775d7e9e
Anisha Bisht <anishabisht76@gmail.com>
2026-06-07 10:08:03 +0530

    second day

---
+++ hoisting.js
@@ -0,0 +1,24 @@
+//let/const behaviour - error unintialized
+// console.log(x);
+// let x = 2;
+
+//undefined cause of execution context
+// console.log(x);
+// var x =2;
+
+//functions works even before initialization
+// console.log(hello);
+// function hello(){
+//     console.log("hello");
+// }
+
+//arrow functions behaviour undefined
+console.log(hello);
+var hello = ()=>{
+    console.log("hello");
+}
+
+
+
+
+
